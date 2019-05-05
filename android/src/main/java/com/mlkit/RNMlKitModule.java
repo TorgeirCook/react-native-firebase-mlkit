0e20448be0ed6da2fdc32a39cbf196f6aa2850d7
Torgeir Pedersen Cook <torgeirpc@gmail.com>
2019-05-05 16:04:36 +0200

    Testing CloudTextRecognizer vs DocumentRecognizer

--- android/src/main/java/com/mlkit/RNMlKitModule.java
+++ android/src/main/java/com/mlkit/RNMlKitModule.java
@@ -1,9 +1,19 @@
 
 package com.mlkit;
 
-import android.graphics.Point;
+import android.app.Activity;
+import android.content.Context;
 import android.graphics.Rect;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
@@ -16,16 +26,36 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
+
+import androidx.annotation.RequiresApi;
+
+import static android.content.Context.CAMERA_SERVICE;
+import static android.hardware.Camera.getCameraInfo;
+import static android.hardware.Camera.getNumberOfCameras;
 
 public class RNMlKitModule extends ReactContextBaseJavaModule {
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    private static final String TAG = "RNMlKit";
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
 
     private final ReactApplicationContext reactContext;
     private FirebaseVisionTextRecognizer textDetector;
@@ -99,17 +129,30 @@ public class RNMlKitModule extends ReactContextBaseJavaModule {
 
     private FirebaseVisionTextRecognizer getCloudTextRecognizerInstance() {
         if (this.cloudTextDetector == null) {
-            this.cloudTextDetector = FirebaseVision.getInstance().getCloudTextRecognizer();
+            FirebaseVisionCloudTextRecognizerOptions options = new FirebaseVisionCloudTextRecognizerOptions.Builder()
+                    .setLanguageHints(Arrays.asList("en"))
+                    .build();
+            this.cloudTextDetector = FirebaseVision.getInstance().getCloudTextRecognizer(options);
         }
 
         return this.cloudTextDetector;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @ReactMethod
-    public void cloudTextRecognition(String uri, final Promise promise) {
+    public void cloudTextRecognition(String uri, final Promise promise) throws CameraAccessException {
         try {
+            int rotation = getRotationCompensation(String.valueOf(getCameraId()), getCurrentActivity(), reactContext.getBaseContext());
+            FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
+                    .setWidth(480)   // 480x360 is typically sufficient for
+                    .setHeight(360)  // image recognition
+                    .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                    .setRotation(rotation)
+                    .build();
+            byte[] bytes = getBytes(uri);
+            FirebaseVisionImage image = FirebaseVisionImage.fromByteBuffer(ByteBuffer.wrap(bytes), metadata);
+
             FirebaseVisionTextRecognizer detector = this.getCloudTextRecognizerInstance();
-            FirebaseVisionImage image = FirebaseVisionImage.fromFilePath(this.reactContext, android.net.Uri.parse(uri));
             Task<FirebaseVisionText> result =
                     detector.processImage(image)
                             .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
@@ -192,9 +235,10 @@ public class RNMlKitModule extends ReactContextBaseJavaModule {
             List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
             info = Arguments.createMap();
             coordinates = Arguments.createMap();
-            FirebaseVisionText.TextBlock textBlock = blocks.get(i);
-            Rect boundingBox = textBlock.getBoundingBox();
+            FirebaseVisionText.TextBlock block = blocks.get(i);
+            Rect boundingBox = block.getBoundingBox();
 
+            coordinates.putDouble("confidenceZ", block.getConfidence());
             coordinates.putInt("top", boundingBox.top);
             coordinates.putInt("bottom", boundingBox.bottom);
             coordinates.putInt("left", boundingBox.left);
@@ -207,10 +251,14 @@ public class RNMlKitModule extends ReactContextBaseJavaModule {
 
             for (int j = 0; j < lines.size(); j++) {
                 List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
-                info.putString("lineText", lines.get(j).getText());
+                FirebaseVisionText.Line line = lines.get(j);
+                info.putString("lineText", line.getText());
+                info.putDouble("lineConfidence", line.getConfidence());
 
                 for (int k = 0; k < elements.size(); k++) {
-                    info.putString("elementText", elements.get(k).getText());
+                    FirebaseVisionText.Element element = elements.get(k);
+                    info.putString("elementText", element.getText());
+                    info.putDouble("elementConfidence", element.getConfidence());
                 }
             }
 
@@ -218,6 +266,78 @@ public class RNMlKitModule extends ReactContextBaseJavaModule {
         }
 
         return data;
+    }
+
+    /**
+     * Get the angle by which an image must be rotated given the device's current
+     * orientation.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private int getRotationCompensation(String cameraId, Activity activity, Context context)
+            throws CameraAccessException {
+        // Get the device's current rotation relative to its "native" orientation.
+        // Then, from the ORIENTATIONS table, look up the angle the image must be
+        // rotated to compensate for the device's rotation.
+        int deviceRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int rotationCompensation = ORIENTATIONS.get(deviceRotation);
+
+        // On most devices, the sensor orientation is 90 degrees, but for some
+        // devices it is 270 degrees. For devices with a sensor orientation of
+        // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
+        CameraManager cameraManager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
+        int sensorOrientation = cameraManager
+                .getCameraCharacteristics(cameraId)
+                .get(CameraCharacteristics.SENSOR_ORIENTATION);
+        rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360;
+
+        // Return the corresponding FirebaseVisionImageMetadata rotation value.
+        int result;
+        switch (rotationCompensation) {
+            case 0:
+                result = FirebaseVisionImageMetadata.ROTATION_0;
+                break;
+            case 90:
+                result = FirebaseVisionImageMetadata.ROTATION_90;
+                break;
+            case 180:
+                result = FirebaseVisionImageMetadata.ROTATION_180;
+                break;
+            case 270:
+                result = FirebaseVisionImageMetadata.ROTATION_270;
+                break;
+            default:
+                result = FirebaseVisionImageMetadata.ROTATION_0;
+                Log.e(TAG, "Bad rotation value: " + rotationCompensation);
+        }
+        return result;
+    }
+
+    private byte[] getBytes(String uri) throws IOException {
+        InputStream inputStream = this.reactContext.getContentResolver().openInputStream(Uri.parse(uri));
+        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+
+        int len = 0;
+        while ((len = inputStream.read(buffer)) != -1) {
+            byteBuffer.write(buffer, 0, len);
+        }
+        return byteBuffer.toByteArray();
+    }
+
+    private int getCameraId() {
+        int cameraId = -1;
+        int numberOfCameras = getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                Log.d(TAG, "Camera found");
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
     }
 
 
